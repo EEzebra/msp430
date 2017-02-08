c730c69bb18c81ec2096ffc506e0e63e9bcb15f7
EEzebra <452370301@qq.com>
2017-02-08 14:33:07 +0800

    Add files via upload

--- AT24C32.h
+++ AT24C32.h
@@ -3,11 +3,13 @@
 #define u32 unsigned long
 
 #define EEP_MAX_PAGE_SIZE  32           //最大页写字节数
-#define EEP_MAX_ROM_SIZE   32768     // EEROM容量
-#define EEP_ADDR_SIZE      2            // EEROM地址字节数
+#define EEP_MAX_ROM_SIZE   32768        //EEROM容量
+#define EEP_ADDR_SIZE      2            //EEROM地址字节数
 
-
+/*24C32擦写时间*/
 #define EEP_WRITE_DELAY_TIME   delay_ms(10)
 
+/*24C32读*/
 void EepromRead(u8 *buf , u16 len , u16 ptr);
+/*24C32写*/
 u8 EepromWrite(u8 *buf , u16 len , u16 ptr);
